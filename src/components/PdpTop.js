299d34814d4553a6ff863cd243c68e024ba1a333
Anthony <duquea95@gmail.com>
2020-10-06 08:32:25 -0400

    Add files

---
+++ src/components/PdpTop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PdpTop = () => {
+
+    return(
+        <div className="pdp-content container-fluid">
+            
+        </div>
+    )
+}
+
+export default PdpTop
